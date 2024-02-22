fdbbe5badf355993cbdad6bd0ded8141e33ef51d
sho1999 <management14sho@gmail.com>
2024-02-22 14:49:36 +0400

    add aleat

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,18 @@
+# app/helpers/application_helper.rb
 module ApplicationHelper
-end
+    def bootstrap_class_for_flash(flash_type)
+      case flash_type
+      when 'success'
+        'alert-success'
+      when 'error'
+        'alert-danger'
+      when 'alert'
+        'alert-warning'
+      when 'notice'
+        'alert-info'
+      else
+        flash_type.to_s
+      end
+    end
+  end
+  
