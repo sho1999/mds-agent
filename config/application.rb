a47f83c6a10a16ea4ac3e2d14ba6bf3e1d1503dc
sho1999 <management14sho@gmail.com>
2024-08-09 09:52:02 +0400

    fix appliction.rb

--- config/application.rb
+++ config/application.rb
@@ -15,6 +15,8 @@ module AgentMgmtApp
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 7.1
 
+    config.hosts << "3.112.60.223" 
+
     # Please, add to the `ignore` list any other `lib` subdirectories that do
     # not contain `.rb` files, or that should not be reloaded or eager loaded.
     # Common ones are `templates`, `generators`, or `middleware`, for example.
