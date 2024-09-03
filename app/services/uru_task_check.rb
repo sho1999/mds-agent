3a0b0148fb829b7d8cad47435552900214738227
sho1999 <management14sho@gmail.com>
2024-09-03 12:06:15 +0400

    add headless mode

--- app/services/uru_task_check.rb
+++ app/services/uru_task_check.rb
@@ -1,12 +1,16 @@
-
-
 require 'watir'
 require 'selenium-webdriver'
 require 'date'
 
 class UruTaskCheck
     def self.check
-        driver = Selenium::WebDriver.for :chrome
+        options = Selenium::WebDriver::Chrome::Options.new
+        options.add_argument('--headless')
+        options.add_argument('--disable-gpu')
+        options.add_argument('--no-sandbox')
+        options.add_argument('--disable-dev-shm-usage')
+
+        driver = Selenium::WebDriver.for :chrome, options: options
 
         # ログイン
         driver.get("https://lim-administration.herokuapp.com/affiliaters/login")
