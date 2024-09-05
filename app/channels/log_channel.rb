bf9c0408357afedfc31b5883cf6bd7d79fe43734
sho1999 <management14sho@gmail.com>
2024-09-05 09:58:01 +0400

    delete cable

--- app/channels/log_channel.rb
+++ app/channels/log_channel.rb
@@ -1,10 +1,10 @@
-class LogChannel < ApplicationCable::Channel
-    def subscribed
-      stream_from "log_channel"
-    end
+# class LogChannel < ApplicationCable::Channel
+#     def subscribed
+#       stream_from "log_channel"
+#     end
   
-    def unsubscribed
-      # Cleanup when channel is unsubscribed
-    end
-  end
+#     def unsubscribed
+#       # Cleanup when channel is unsubscribed
+#     end
+#   end
   
--- app/javascript/packs/connect.js
+++ app/javascript/packs/connect.js
@@ -1,26 +1,26 @@
-// Import ActionCable from the @rails/actioncable package
-import { createConsumer } from "@rails/actioncable";
+// // Import ActionCable from the @rails/actioncable package
+// import { createConsumer } from "@rails/actioncable";
 
-// Create a consumer and assign it globally to the window object
-window.ActionCable = createConsumer();
+// // Create a consumer and assign it globally to the window object
+// window.ActionCable = createConsumer();
 
-document.addEventListener("DOMContentLoaded", function () {
+// document.addEventListener("DOMContentLoaded", function () {
 
-    // Use ActionCable to create a subscription to the LogChannel
-    const cable = window.ActionCable;
+//     // Use ActionCable to create a subscription to the LogChannel
+//     const cable = window.ActionCable;
 
-    cable.subscriptions.create("LogChannel", {
-        received(data) {
-            const logContainer = document.getElementById("log-container");
-            const logMessage = document.createElement("p");
-            logMessage.textContent = data.message;
+//     cable.subscriptions.create("LogChannel", {
+//         received(data) {
+//             const logContainer = document.getElementById("log-container");
+//             const logMessage = document.createElement("p");
+//             logMessage.textContent = data.message;
 
-            // Add this to make the text align to the left
-            logMessage.style.textAlign = "left";
+//             // Add this to make the text align to the left
+//             logMessage.style.textAlign = "left";
 
-            logContainer.appendChild(logMessage);
-            logContainer.scrollTop = logContainer.scrollHeight; // Automatically scroll to the bottom
-        },
-    });
-});
+//             logContainer.appendChild(logMessage);
+//             logContainer.scrollTop = logContainer.scrollHeight; // Automatically scroll to the bottom
+//         },
+//     });
+// });
 
